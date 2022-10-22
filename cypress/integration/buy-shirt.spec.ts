fd06f1aaf2bc501fcc226fb3c5f1565a1c17b251
luisdanielvcord <luisdanielvasquez@hotmail.es>
2022-10-22 16:31:22 -0500

    Fixing AAA buy-shirt.spec.ts

--- cypress/integration/buy-shirt.spec.ts
+++ cypress/integration/buy-shirt.spec.ts
@@ -8,31 +8,35 @@ import {
   ShoppingCartPage,
 } from "../page/index";
 
-const menuContentPage = new MenuContentPage();
-const productLists = new ProductsListPage();
-const shoppingCartPage = new ShoppingCartPage();
-const loginPage = new LoginPage();
-const addressStepPage = new AddressStepPage();
-const shippingStepPage = new ShippingStepPage();
-const paymentStepPage = new PaymentStepPage();
-
 describe("Buy a t-shirt", () => {
+  let menuContentPage: MenuContentPage;
+  let productLists: ProductsListPage;
+  let shoppingCartPage: ShoppingCartPage;
+  let loginPage: LoginPage;
+  let addressStepPage: AddressStepPage;
+  let shippingStepPage: ShippingStepPage;
+  let paymentStepPage: PaymentStepPage;
+
+  before(() => {
+    menuContentPage = new MenuContentPage();
+    productLists = new ProductsListPage();
+    shoppingCartPage = new ShoppingCartPage();
+    loginPage = new LoginPage();
+    addressStepPage = new AddressStepPage();
+    shippingStepPage = new ShippingStepPage();
+    paymentStepPage = new PaymentStepPage();
+  });
+
   it("then should be bought a t-shirt", () => {
     menuContentPage.visitMenuContentPage();
     menuContentPage.goToTShirtMenu();
-
     productLists.addShirtToCart();
     productLists.proceedToCheckout();
-
     shoppingCartPage.proceedToCheckout();
-
     loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor");
-
     addressStepPage.proceedToCheckout();
-
     shippingStepPage.acceptTerms();
     shippingStepPage.proceedToCheckout();
-
     paymentStepPage.selectBankWire();
     paymentStepPage.confirmOrder();
 
